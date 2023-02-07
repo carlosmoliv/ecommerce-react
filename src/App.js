f192ef324344326e0906de2191882fc20fe19d36
Carlos Oliveira <carlosmoliveira93@gmail.com>
2023-02-06 23:48:12 -0300

    finish section 5 and starting firebase config

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
-import CategoriesList from "./components/categories-list/categories-list.component";
+import Home from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigation.component";
+import { Routes, Route } from "react-router-dom";
 
-const App = () => <CategoriesList />;
+const Shop = () => {
+  return <h1>Shop page</h1>;
+};
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+      </Route>
+    </Routes>
+  );
+};
 
 export default App;
