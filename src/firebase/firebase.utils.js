a8a455bb2b87294c64c35747190783eae80c1f04
Carlos Oliveira <carlosmoliveira93@gmail.com>
2023-02-09 14:49:53 -0300

    add firebase listener

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -7,6 +7,7 @@ import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
   signOut,
+  onAuthStateChanged,
 } from "firebase/auth";
 
 import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
@@ -83,3 +84,5 @@ export const createAuthUserWithEmail = async (email, password) => {
 
   return createUser;
 };
+
+export const onAuthListener = (callback) => onAuthStateChanged(auth, callback);
